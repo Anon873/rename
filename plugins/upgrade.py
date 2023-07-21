a191782493e8ee452e994cb4c63538a9581b2b5a
Virat jaat <126441859+Anon873@users.noreply.github.com>
2023-07-21 20:39:54 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -5,30 +5,30 @@ from pyrogram import Client , filters
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
 	text = """**Free Plan User**
-	Daily  Upload limit 1.2GB
+	Daily  Upload limit 2GB
 	Price 0
 	
 	**🪙 Silver Tier 🪙** 
-	Daily  Upload  limit 10GB
-	Price Rs 66  ind /🌎 0.8$  per Month
+	Daily  Upload  limit 30GB
+	Price Rs 20  ind /🌎 0.5$ per Month
 	
 	**💫 Gold Tier 💫**
-	Daily Upload limit 50GB
-	Price Rs 100  ind /🌎 1.2$  per Month
+	Daily Upload limit 300GB
+	Price Rs 50  ind /🌎 1$ per Month
 	
 	**💎 Diamond 💎**
-	Daily Upload limit 100GB
-	Price Rs 206  ind /🌎 2.5$  per Month
+	Daily Upload limit 1000GB 
+	Price Rs 100  ind /🌎 2$ per Month
 	
 	
-	Pay Using Upi I'd ```7808912076@paytm```
+	Pay Using Upi I'd ```abhidarkside01@paytm```
 	
 	After Payment Send Screenshots Of 
         Payment To Admin @mRiderDM"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mRiderDM")], 
-        			[InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/ab0hii")], 
+        			[InlineKeyboardButton("Paytm",url = "Contact admin for payment"),
+        			InlineKeyboardButton("Btc",url = "Contact admin for payment")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
@@ -40,23 +40,23 @@ async def upgradecm(bot,message):
 	
 	**🪙 Silver Tier 🪙** 
 	Daily  Upload  limit 10GB
-	Price Rs 66  ind /🌎 0.8$  per Month
+	Price Rs 20  ind /🌎 0.5$ per Month
 	
 	**💫 Gold Tier 💫**
-	Daily Upload limit 50GB
-	Price Rs 100  ind /🌎 1.2$  per Month
+	Daily Upload limit 300GB
+	Price Rs 50  ind /🌎 1$ per Month
 	
 	**💎 Diamond 💎**
 	Daily Upload limit 100GB
-	Price Rs 206  ind /🌎 2.5$  per Month
+	Price Rs 100  ind /🌎 2$ per Month
 	
 	
-	Pay Using Upi I'd ```7808912076@paytm```
+	Pay Using Upi I'd ```(kindly contact admin)```
 	
 	After Payment Send Screenshots Of 
         Payment To Admin @mRiderDM"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mRiDerDM")], 
-        			[InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
-	await message.reply_text(text = text,reply_markup = keybord)
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/ab0hii")], 
+        			[InlineKeyboardButton("Paytm",url = "Contact admin for payment"),
+        			InlineKeyboardButton("Btc",url = "Contact admin for payment")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await update.message.edit(text = text,reply_markup = keybord)

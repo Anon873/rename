76b0bb88b7428a6b872c0131ef98ca624e09d2e2
Virat jaat <126441859+Anon873@users.noreply.github.com>
2023-07-25 15:41:59 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -5,7 +5,7 @@ from pyrogram import Client , filters
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
 	text = """**Free Plan User**
-	Daily  Upload limit 10GB
+	Daily  Upload limit 5GB
 	(Free hai sur)
 	
 	**🪙 Silver Tier 🪙** 
@@ -31,7 +31,7 @@ async def upgrade(bot,update):
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
 	text = """**Free Plan User**
-	Daily  Upload limit 10GB
+	Daily  Upload limit 5GB
 	(Free hai sur)
 	
 	**🪙 Silver Tier 🪙** 

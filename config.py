f5c8ccacb5a3a10ce7743f83d8ed42c06b5490f9
Virat jaat <126441859+Anon873@users.noreply.github.com>
2023-07-27 15:55:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,12 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
     STRING = os.environ.get("STRING", "")
+    DB_NAME = os.environ.get("DB_NAME", "")
+    DB_URL = os.environ.get("DB_URL", "")
+    ADMIN = os.environ.get("ADMIN", "")
+    CHANNEL = os.environ.get("CHANNEL", "")
+    LAZY_PIC = os.environ.get("LAZY_PIC", "")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
 
     
     

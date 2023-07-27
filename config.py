498756189d287fa75090acc8e0be5fc8b31aa334
Virat jaat <126441859+Anon873@users.noreply.github.com>
2023-07-27 15:51:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,11 @@ import os
 
 class Config(object):
     START_PIC   = os.environ.get("START_PIC", "")
-    
+    TOKEN = os.environ.get("TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
+    STRING = os.environ.get("STRING", "")
+
     
     
     
